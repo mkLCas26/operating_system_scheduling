4bcb99464a6c8ad51bbf328ebb42df603f415d2b
Kristina Cas <m.kristinacas@gmail.com>
2026-06-27 02:30:14 +0800

    feat(lfu_rep_logic): finished and added lfu logic code

---
+++ virtual_memory/lfu_rep_logic.py
@@ -0,0 +1,54 @@
+''' For Least Frequently Used Page Replacement Algorithm logic '''
+
+class LFURep:
+    def __init__(self, ref_str, fcount):
+        self.ref_str = ref_str
+        self.fcount = fcount
+    
+    def run(self):
+        steps = []
+        frames = []
+        hits  = 0
+        faults = 0
+        count = {}
+        fifo_fallback = {}
+        
+        for i, page in enumerate(self.ref_str):
+            count[page] = count.get(page, 0) + 1
+            
+            if page in frames:
+                hits += 1
+                steps.append({
+                    "page": page,
+                    "frames": frames.copy(),
+                    "status": "HIT"
+                })
+                
+            else:
+                faults += 1
+                if len(frames) < self.fcount:
+                    frames.append(page)
+                    fifo_fallback[page] = i
+                    
+                else:
+                    min_count = min(count[f] for f in frames)
+                    check = [f for f in frames if count[f] == min_count]
+                    
+                    target = min(check, key=lambda c: fifo_fallback[c])
+                    
+                    target = frames.index(target)
+                    frames[target] = page
+                    fifo_fallback[page] = i
+                
+                steps.append({
+                    "page": page,
+                    "frames": frames.copy(),
+                    "status": "FAULT"
+                })
+                
+        return{
+            "fault_total": faults,
+            "hit_total": hits,
+            "steps": steps
+        }
+                 
