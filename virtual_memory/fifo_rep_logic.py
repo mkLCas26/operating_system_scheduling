d6559356426ffc160ab5b9761e1922012bd99411
Kristina Cas <m.kristinacas@gmail.com>
2026-06-27 02:24:32 +0800

    feat(fifo_rep_logic): refined and finished logic code for fifo

--- virtual_memory/fifo_rep_logic.py
+++ virtual_memory/fifo_rep_logic.py
@@ -11,3 +11,39 @@ class FIFORep:
         frames =[]
         hits = 0
         faults = 0
+        
+        for page in self.ref_str:
+            if page in frames:
+                hits += 1
+                steps.append({
+                    "page": page,
+                    "frames": frames.copy(),
+                    "status": "HIT"
+                })
+                
+            else:
+                faults += 1
+                
+                if len(frames) < self.fcount:
+                    frames.append(page)
+                    queue.append(page)
+                    
+                else:
+                    oldest_page = queue.pop(0)
+                    target = frames.index(oldest_page)
+                    frames[target] = page
+                    queue.append(page)
+                    
+                
+                steps.append({
+                    "page": page,
+                    "frames": frames.copy(),
+                    "status": "FAULT"
+                })
+        
+        return{
+            "fault_total": faults,
+            "hit_total": hits,
+            "steps": steps
+        }
+                
